74b8468bc05b485fe914d6811b09e9bab8aba38e
Luiz Ferreira <luizferreira91@gmail.com>
2020-04-14 18:09:05 -0300

    StoneMasonKarel first solution with comments

--- StoneMasonKarel.py
+++ StoneMasonKarel.py
@@ -11,14 +11,47 @@ sample worlds supplied in the starter folder.
 
 
 def main():
-    """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
-    """
-    pass
+    repair_column()
 
+
+#Do the repair in the column if Karel is facing east
+#After the repair she go back to her initial position
+def repair_column():
+    while facing_east():
+        turn_left()
+        while front_is_clear():
+            if beepers_present():
+                move()
+            else:
+                put_beeper()
+        going_back()
+
+
+# Going back to her initial position
+# If she encounter a wall in front of her, she will turn left to don't face east
+# I need to improve that but was a solution that I've found.
+def going_back():
+    if no_beepers_present():
+        put_beeper()
+    turn_around()
+    while front_is_clear():
+        move()
+    turn_left()
+    if front_is_clear():
+        next_column()
+    else:
+        turn_left()
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+
+# Go to the next column
+def next_column():
+    for i in range(4):
+        move()
 
 # There is no need to edit code beyond this point
 
