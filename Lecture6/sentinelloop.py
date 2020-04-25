f979618713c347b62d84acf32db1183438d8ac8e
Luiz Ferreira <luizferreira91@gmail.com>
2020-04-25 19:43:03 -0300

    sentinel loop challenge

--- Lecture6/sentinelloop.py
+++ Lecture6/sentinelloop.py
@@ -1,13 +1,23 @@
 """
 File: sentinelloop.py
 ------------------
-Simulate rolling two dice, and prints results of each
-roll as well as the total.
+A program that prompts the user for numbers until the user types -1, then output the total
+of the numbers.
 """
+SENTINEL = -1
+
 
 def main():
     total = 0
-    print(total)
+    number = int(input('Type a number: '))
+
+    # The while loop is responsible for compare the variable number with de constant SENTINEL
+    # post condition: The program needs to keep making the same question until the user types the SENTINEL value.
+    while number != SENTINEL:
+        total = total + number
+        number = int(input('Type a number: '))
+    print('The total is ' + str(total))
+
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
