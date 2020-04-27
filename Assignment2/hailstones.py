4271addc012c29fa18b7724083ee28a48efcc68e
Luiz Ferreira <luizferreira91@gmail.com>
2020-04-27 18:38:29 -0300

    hailstones done and khansole fix

--- Assignment2/hailstones.py
+++ Assignment2/hailstones.py
@@ -7,13 +7,26 @@ you'd like to try solving it.
 
 
 def main():
-    """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
-    """
-    pass
+
+    # Pick some positive integer and call it n.
+    num1 = int(input('Enter a positive number: '))
+    while num1 <= 0:
+        num1 = int(input('Invalid number, please enter a positive number: '))
+
+    # Continue this process until n is equal to one.
+    while num1 != 1:
+        # I used this base variable to store the original value of num1 each loop.
+        base = num1
+
+        # If n is even, divide it by two.
+        if (num1 % 2) == 0:
+            num1 = int((num1 / 2))
+            print(str(base) + ' is even, so I take half: ' + str(num1))
+
+        # If n is odd, multiply it by three and add one.
+        elif (num1 % 2) != 0:
+            num1 = int((num1 * 3) + 1)
+            print(str(base) + ' is odd, so I make 3n + 1: ' + str(num1))
 
 
 # This provided line is required at the end of a Python file
--- Assignment2/khansole_academy.py
+++ Assignment2/khansole_academy.py
@@ -16,6 +16,26 @@ MAX_RANDOM = 99
 
 
 def main():
+    answer = int(input('WELCOME TO KHANSOLE ACADEMY\n1- Addition\n2- Subtraction\n3- Division\n4- Multiplication\n'
+                       'What do you want to practice?: '))
+
+    # While the user does not select a valid answer the program will ask a new question
+    while answer < 0 or answer > 4:
+        print('Invalid option\n')
+        answer = int(input('WELCOME TO KHANSOLE ACADEMY\n1- Addition\n2- Subtraction\n3- Division\n4- Multiplication\n'
+                           'What do you want to practice?: '))
+    if answer == 1:
+        addition()
+    elif answer == 2:
+        subtraction()
+    elif answer == 3:
+        division()
+    elif answer == 4:
+        multiplication()
+
+
+# Addition function
+def addition():
     # I started with a variable to count the correct answers the user gave to the program
     correct_answers = 0
     # Then I used a while loop to compare the correct answers with the row that the user needs to master the subject
@@ -25,8 +45,10 @@ def main():
         num1 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
         num2 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
         addition = (num1 + num2)
+
         # Print a question with two random numbers between MIN_RANDOM and MAX_RANDOM
         answer = int((input('What is ' + str(num1) + '+' + str(num2) + ' ?: ')))
+
         # Check if the answer is correct or not and print the result
         if answer == addition:
             # If the answer is correct the program will add 1 unit into the correct_answers variable
@@ -41,6 +63,100 @@ def main():
     # When the user gives 3 corrects answers in a row, finish the program and congrats the user
     if correct_answers == CORRECT_ROW:
         print('Congratulations! You mastered addition!')
+
+
+# Subtraction function
+def subtraction():
+    # I started with a variable to count the correct answers the user gave to the program
+    correct_answers = 0
+    # Then I used a while loop to compare the correct answers with the row that the user needs to master the subject
+    while correct_answers != CORRECT_ROW:
+        # I created two variables to generate the random numbers, it's important to put the variables inside the loop
+        # If they are outside the program will use the same numbers
+        num1 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
+        num2 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
+        subtraction = (num1 - num2)
+
+        # Print a question with two random numbers between MIN_RANDOM and MAX_RANDOM
+        answer = int((input('What is ' + str(num1) + '-' + str(num2) + ' ?: ')))
+
+        # Check if the answer is correct or not and print the result
+        if answer == subtraction:
+            # If the answer is correct the program will add 1 unit into the correct_answers variable
+            correct_answers += 1
+            print("Correct! You've gotten " + str(correct_answers) + " correct in a row")
+        else:
+            # If the answer is incorrect, the program will print a message and will define the value of 0
+            # The program needs to do that to start the count again if the user can't reach 3 corrects answers in a row
+            print("Incorrect. The expected answer is " + str(subtraction))
+            correct_answers = 0
+
+    # When the user gives 3 corrects answers in a row, finish the program and congrats the user
+    if correct_answers == CORRECT_ROW:
+        print('Congratulations! You mastered addition!')
+
+
+# Division function
+def division():
+    # I started with a variable to count the correct answers the user gave to the program
+    correct_answers = 0
+    # Then I used a while loop to compare the correct answers with the row that the user needs to master the subject
+    while correct_answers != CORRECT_ROW:
+        # I created two variables to generate the random numbers, it's important to put the variables inside the loop
+        # If they are outside the program will use the same numbers
+        num1 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
+        num2 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
+        division = (num1 / num2)
+
+        # Print a question with two random numbers between MIN_RANDOM and MAX_RANDOM
+        answer = int((input('What is ' + str(num1) + '/' + str(num2) + ' ?: ')))
+
+        # Check if the answer is correct or not and print the result
+        if answer == division:
+            # If the answer is correct the program will add 1 unit into the correct_answers variable
+            correct_answers += 1
+            print("Correct! You've gotten " + str(correct_answers) + " correct in a row")
+        else:
+            # If the answer is incorrect, the program will print a message and will define the value of 0
+            # The program needs to do that to start the count again if the user can't reach 3 corrects answers in a row
+            print("Incorrect. The expected answer is " + str(division))
+            correct_answers = 0
+
+    # When the user gives 3 corrects answers in a row, finish the program and congrats the user
+    if correct_answers == CORRECT_ROW:
+        print('Congratulations! You mastered addition!')
+
+
+# Multiplication function
+def multiplication():
+    # I started with a variable to count the correct answers the user gave to the program
+    correct_answers = 0
+    # Then I used a while loop to compare the correct answers with the row that the user needs to master the subject
+    while correct_answers != CORRECT_ROW:
+        # I created two variables to generate the random numbers, it's important to put the variables inside the loop
+        # If they are outside the program will use the same numbers
+        num1 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
+        num2 = int(random.randint(MIN_RANDOM, MAX_RANDOM))
+        multiplication = (num1 * num2)
+
+        # Print a question with two random numbers between MIN_RANDOM and MAX_RANDOM
+        answer = int((input('What is ' + str(num1) + '*' + str(num2) + ' ?: ')))
+
+        # Check if the answer is correct or not and print the result
+        if answer == multiplication:
+            # If the answer is correct the program will add 1 unit into the correct_answers variable
+            correct_answers += 1
+            print("Correct! You've gotten " + str(correct_answers) + " correct in a row")
+        else:
+            # If the answer is incorrect, the program will print a message and will define the value of 0
+            # The program needs to do that to start the count again if the user can't reach 3 corrects answers in a row
+            print("Incorrect. The expected answer is " + str(multiplication))
+            correct_answers = 0
+
+    # When the user gives 3 corrects answers in a row, finish the program and congrats the user
+    if correct_answers == CORRECT_ROW:
+        print('Congratulations! You mastered addition!')
+
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
